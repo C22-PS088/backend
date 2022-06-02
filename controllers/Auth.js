530361013dcd677252bc1649676a37b012936070
Setiawan <uyab.elek878@gmail.com>
2022-06-03 00:42:53 +0700

    Changed httpOnly: true to false in Login set cookie

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -34,7 +34,7 @@ const Login = async (req, res) => {
     });
 
     res.cookie('refreshToken', refreshToken, {
-      httpOnly: true,
+      httpOnly: false,
       maxAge: 24 * 60 * 60 * 1000
       // , secure: true
     });
